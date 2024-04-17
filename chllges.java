afe605a84523ca9fb309e5bced5af6ea2692b172
Your Name <you@example.com>
2024-04-17 11:26:43 +0100

    realiser le challenge java

---
+++ chllges.java
@@ -0,0 +1,55 @@
+// chllg 1
+// Use this editor to write, compile and run your Java code online
+
+
+    public static void main(String[] args) {
+        int a,b;
+        sum==(3'+'2);
+        System.out.println("a+b");
+    }
+}
+
+//chll 5
+
+    
+    public static void main(string[] args){
+        envoyer("nom");
+        system.out.println("Hello Gerald!")
+    }
+}
+//chll3
+
+    int nbr;
+    rslt(tnbr);
+    
+    
+}
+//chll 6
+
+getFirst();
+return first;
+//chll4
+isSameNum(4,8);
+
+if(x==y){
+    return true;
+}
+else{
+    return false;
+}
+}
+//chll 2
+int n;
+convert(5);
+min*60==sec;
+//chll 7
+acceptIntoMovie();
+if(pers>=15+){
+    return true;
+}
+else {   return false
+}
+
+//chll 8
+countWords();
+}
